6da20b46ab0f7d4e0f82079fc661a49231f9caff
renatamudarisova83-max <renatamudarisova83@gmail.com>
2025-09-23 21:26:53 +0500

    написано все

--- star-n.py
+++ star-n.py
@@ -3,6 +3,8 @@ from telebot import types
 import webbrowser
 
 
+
+
 bot=telebot.TeleBot('8466977919:AAH9aIL-BK6zP45iktikLGwLQ01oSCzTaj4')
 @bot.message_handler(commands=['isu'])
 # def ISU(message):
